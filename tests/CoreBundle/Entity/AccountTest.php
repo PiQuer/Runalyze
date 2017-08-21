abeac528b39dd51699dac4bdb788de9b7be715ae
Michael Pohl <michael@runalyze.com>
2017-08-21 22:16:34 +0200

    update tests

--- tests/CoreBundle/Entity/AccountTest.php
+++ tests/CoreBundle/Entity/AccountTest.php
@@ -17,34 +17,7 @@ class AccountTest extends \PHPUnit_Framework_TestCase
     public function testDefaultValues()
     {
         $this->assertNotNull($this->Account->getSalt());
-
-        $this->assertNull($this->Account->getChangepwHash());
-        $this->assertNull($this->Account->getChangepwTimelimit());
-        $this->assertNull($this->Account->getActivationHash());
-        $this->assertNull($this->Account->getDeletionHash());
     }
 
-    public function testChangePasswordHash()
-    {
-        $this->Account->setNewChangePasswordHash();
 
-        $this->assertNotNull($this->Account->getChangepwHash());
-        $this->assertNotNull($this->Account->getChangepwTimelimit());
-
-        $this->Account->removeChangePasswordHash();
-
-        $this->assertNull($this->Account->getChangepwHash());
-        $this->assertNull($this->Account->getChangepwTimelimit());
-    }
-
-    public function testActivationHash()
-    {
-        $this->Account->setNewActivationHash();
-
-        $this->assertNotNull($this->Account->getActivationHash());
-
-        $this->Account->removeActivationHash();
-
-        $this->assertNull($this->Account->getActivationHash());
-    }
 }
