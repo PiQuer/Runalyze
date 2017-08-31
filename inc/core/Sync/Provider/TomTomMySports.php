627467739e6154711feaa4cf7adb4eeebd752cbc
Michael Pohl <michael@runalyze.com>
2017-08-31 07:26:40 +0200

    local changes

--- inc/core/Sync/Provider/TomTomMySports.php
+++ inc/core/Sync/Provider/TomTomMySports.php
@@ -1,5 +1,8 @@
 <?php
 namespace Runalyze\Sync\Provider;
+use Runalyze\Bundle\CoreBundle\Entity\AccountClient;
+use Runalyze\Bundle\CoreBundle\Entity\AccountClientRepository;
+use Runalyze\Bundle\CoreBundle\Entity\AccountClientSettingsRepository;
 use Runalyze\Sync\Provider\ActivitySyncInterface;
 use Runalyze\Profile\Sync\SyncProvider;
 use League\OAuth2\Client\Provider;
@@ -7,32 +10,38 @@ use KnpU\OAuth2ClientBundle\Client\OAuth2Client;
 
 class TomTomMySports implements ActivitySyncInterface {
 
-    /*
-     * @var Provider\TomTomMySports
-     */
+    /** @var Provider\TomTomMySports */
     protected $client;
 
-    /*
-     * @var string $refreshToken
-     */
+    /** @var string $refreshToken */
     protected $refreshToken;
 
-    /*
-     * @var string $refreshToken
-     */
+    /** @var string $refreshToken */
     protected $accessToken;
+
+    /** @var AccountClientRepository */
+    protected $AccountClientRepository;
+
+    /** @var AccountClientSettingsRepository */
+    protected $AccountClientSettingsRepository;
 
 
     /**
      * Constructor
+     *
+     * @param AccountClientRepository $accountClient
+     * @param AccountClientSettingsRepository $accountClient
      * @param OAuth2Client $client
-     * @param string $refreshToken
      */
-    public function __construct(OAuth2Client $client, $refreshToken)
+    public function __construct(AccountClientRepository $accountClient, AccountClientSettingsRepository $accountClientSettings, OAuth2Client $client)
     {
+        $this->AccountClientRepository = $accountClient;
+        $this->AccountClientSettingsRepository = $accountClientSettings;
         $this->client = $client;
-        $this->accessToken = $refreshToken;
-        $this->getAccessToken();
+    }
+
+    public function setRefreshToken($refreshToken) {
+        $this->refreshToken = $refreshToken;
     }
 
     public static function getIdentifier() {
@@ -40,10 +49,12 @@ class TomTomMySports implements ActivitySyncInterface {
     }
 
     private function getAccessToken() {
-        $accessToken = $this->client->getAccessToken('refresh_token', [
-            'refresh_token' => $this->refreshToken
-        ]);
-        $this->accessToken = $accessToken->getToken();
+        if (empty($accessToken)) {
+            $accessToken = $this->client->getAccessToken('refresh_token', [
+                'refresh_token' => $this->refreshToken
+            ]);
+            $this->accessToken = $accessToken->getToken();
+        }
 
     }
 
@@ -51,6 +62,7 @@ class TomTomMySports implements ActivitySyncInterface {
      * @link https://developer.tomtom.com/tomtom-sports-cloud/tomtom-sports-cloud-documentation/get-activity-list
      */
     public function fetchActivityList() {
+        $this->getAccessToken();
 
         $url = Provider\TomTomMySports::BASE_MYSPORTS_URL.'1/activity';
         /** @var Provider\TomTomMySports */
