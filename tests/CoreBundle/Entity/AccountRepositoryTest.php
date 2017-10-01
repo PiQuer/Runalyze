4df376734c63e8ceeed8c55c48179515b22d0336
Michael Pohl <michael@runalyze.com>
2017-10-01 10:44:08 +0200

    fix unittest (count activated users)

--- tests/CoreBundle/Entity/AccountRepositoryTest.php
+++ tests/CoreBundle/Entity/AccountRepositoryTest.php
@@ -70,7 +70,7 @@ class AccountRepositoryTest extends AbstractRepositoryTestCase
             $this->EntityManager->persist($this->getNewAccount($name));
         }
 
-        $this->EntityManager->persist($this->getNewAccount('foobar')->setStatus(AccountStatusProfile::DISABLEDs));
+        $this->EntityManager->persist($this->getNewAccount('foobar')->setStatus(AccountStatusProfile::DISABLED));
         $this->EntityManager->flush();
 
         $this->assertEquals($activeAccounts + 3, $this->AccountRepository->getAmountOfActivatedUsers(false));
