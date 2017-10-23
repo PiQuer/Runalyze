25ae1ad21a3910d739a97e40784e56470ed44e83
Michael Pohl <michael@runalyze.com>
2017-10-23 21:16:55 +0200

    minor fixes

--- src/CoreBundle/Controller/Connect/TomTomConnectController.php
+++ src/CoreBundle/Controller/Connect/TomTomConnectController.php
@@ -13,7 +13,6 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Runalyze\Profile\Sync\SyncProviderProfile;
 use Runalyze\Bundle\CoreBundle\Entity\Notification;
 use Runalyze\Bundle\CoreBundle\Entity\Account;
 
